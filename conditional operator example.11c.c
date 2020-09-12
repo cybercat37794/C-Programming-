dc60650134b8c2e218ac60a0c8e657af4ef6ab0a
cybercat37794 <68174387+cybercat37794@users.noreply.github.com>
2020-09-12 19:24:49 +0600

    Add files via upload

---
+++ conditional operator example.11c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+  int a = 9,b = 4;
+  int result= a+b;
+  printf("a+b = %d \n",result);
+  result= a-b;
+  printf("a-b = %d \n",result);
+  result= a*b;
+  printf("a*b = %d \n",result);
+  result=a/b;
+  printf("a/b = %d \n",result);
+  result=a%b;
+  printf("Remainder when a divided by b = %d \n",result);
+   return 0;
+}
---
+++ conditional operator example2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a =500,b,c;
+    if (a>=400);
+    b=300;
+    c=200;
+    printf("\n %d",b,c);
+    return 0;
+}
+
