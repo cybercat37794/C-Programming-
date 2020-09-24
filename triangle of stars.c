49e49fdd2d006bbe6ad6f88a13f3f4bdeb3a9ef9
cybercat37794 <68174387+cybercat37794@users.noreply.github.com>
2020-09-24 20:28:52 +0600

    Add files via upload

---
+++ triangle of stars.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+printf("printing triangle of stars\n");
+printf("*\n");
+printf("**\n");
+printf("***\n");
+printf("****\n");
+printf("*****\n");
+printf("******\n");
+return 0;
+}
