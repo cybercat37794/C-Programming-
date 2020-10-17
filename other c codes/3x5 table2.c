7ffb69ff9bcc856f86667588d4026e75d6002500
cybercat37794 <68174387+cybercat37794@users.noreply.github.com>
2020-10-17 21:17:48 +0600

    Add files via upload

---
+++ other c codes/3x5 table2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+printf("*\t*\t*\n");
+//t is used to print tab//
+printf("**\t**\t**\n");
+printf("***\t***\t***\n");
+printf("****\t****\t****\n");
+printf("*****\t*****\t*****\n");
+return 0;
+
+}
